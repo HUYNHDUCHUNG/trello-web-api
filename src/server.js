45bcc4a8cdb943e2c08d9a287eb07242b9d5abcc
Hung <huynhhung2108@gmail.com>
2024-01-20 13:53:20 +0700

    init code base

--- src/server.js
+++ src/server.js
@@ -1,14 +1,35 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ */
+
 import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
 
 const hostname = 'localhost'
-const port = '8012'
+const port = 8017
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(
+    mapOrder(
+      [
+        { id: 'id-1', name: 'One' },
+        { id: 'id-2', name: 'Two' },
+        { id: 'id-3', name: 'Three' },
+        { id: 'id-4', name: 'Four' },
+        { id: 'id-5', name: 'Five' }
+      ],
+      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+      'id'
+    )
+  )
+  res.end('<h1>Hello World!</h1><hr>')
+})
 
-app.listen(port,hostname,() =>{
-    console.log(`Server runing at http://${hostname}:${port}/`)
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running at http://${hostname}:${port}/`)
 })
