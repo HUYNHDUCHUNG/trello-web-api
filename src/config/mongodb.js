f18069b191628abfa5279fa8b318b7901f893b16
Hung <huynhhung2108@gmail.com>
2024-01-21 00:05:48 +0700

    connect mongoDB

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,26 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+const MONGODB_URI =
+  'mongodb+srv://huynhhung2108:k7smVleWm1E8yTvd@cluster0.rjlmaqg.mongodb.net/?retryWrites=true&w=majority'
+const DATABASE_NAME = 'trello'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+const clientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await clientInstance.connect()
+
+  trelloDatabaseInstance = clientInstance.db(DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('must connect to Database first')
+  return trelloDatabaseInstance
+}
--- src/server.js
+++ src/server.js
@@ -1,35 +1,28 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- */
-
+// eslint-disable-next-line no-console
 import express from 'express'
 import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, GET_DB } from '~/config/mongodb'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  const hostname = 'localhost'
+  const port = 8017
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(
-    mapOrder(
-      [
-        { id: 'id-1', name: 'One' },
-        { id: 'id-2', name: 'Two' },
-        { id: 'id-3', name: 'Three' },
-        { id: 'id-4', name: 'Four' },
-        { id: 'id-5', name: 'Five' }
-      ],
-      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-      'id'
-    )
-  )
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray())
+    res.end('<h1>Hello World!</h1><hr>')
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+  app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+  })
+}
+
+CONNECT_DB()
+  .then(() => console.log('Connected to MongoDB'))
+  .then(() => START_SERVER())
+  .catch((error) => {
+    console.error(error)
+    process.exit(0)
+  })
